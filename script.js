3ce2129abef8aca97dbdf030bafef3ad19c8ea9f
vikas1509 <119025906+vikas1509@users.noreply.github.com>
2023-07-30 21:25:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,16 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	let k = object.keys(sampleObject);
+	k.forEach((item)=>{
+		if(item==key){
+			return true;
+		}
+	})
+	
 }
 
-// Do not change the code below
+Do not change the code below
 const key = prompt("Enter Key.");
 alert(hasKey(key));
+
