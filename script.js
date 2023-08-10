923d635bf3b9e6515c485d8bf97577d501a1c157
vikas1509 <119025906+vikas1509@users.noreply.github.com>
2023-08-10 09:50:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
-  //   write your code here
-	let k = object.keys(sampleObject);
-	k.forEach((item)=>
-	
-		if(item==key){
-		prompt("true")
-		}
-		else{prompt("true")}
-		
-	)
-
-	
-	
+  return key in sampleObject;
 }
 
-//Do not change the code below
-const key = prompt("Enter Key.");
-alert(hasKey(key));
+// Get user input
+const userInput = prompt("Enter a key to check:");
+
+// Check if the key exists in the sampleObject
+const keyExists = hasKey(userInput);
+
+// Display the result
+if (keyExists) {
+  console.log(`The key "${userInput}" exists in the sampleObject.`);
+} else {
+  console.log(`The key "${userInput}" does not exist in the sampleObject.`);
+}
+In this code, the prompt function is used to get user input. Then, the hasKey function is used to check if the provided key exists in the sampleObject. Finally, the code displays a message based on whether the key exists or not.
+
+
+
+
 
