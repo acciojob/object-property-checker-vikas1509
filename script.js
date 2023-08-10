3ac91a7fe0b895eb81d77d6b934ab90508ec42ff
vikas1509 <119025906+vikas1509@users.noreply.github.com>
2023-08-10 09:51:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -16,9 +16,3 @@ if (keyExists) {
 } else {
   console.log(`The key "${userInput}" does not exist in the sampleObject.`);
 }
-In this code, the prompt function is used to get user input. Then, the hasKey function is used to check if the provided key exists in the sampleObject. Finally, the code displays a message based on whether the key exists or not.
-
-
-
-
-
